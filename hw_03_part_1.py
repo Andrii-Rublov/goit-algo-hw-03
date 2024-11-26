6bcc9fe8097a20110aac33d9e320ffea79e96468
Andrii-Rublov <rublyovandrew@gmail.com>
2024-11-26 21:19:04 +0200

    corrected

--- hw_03_part_1.py
+++ hw_03_part_1.py
@@ -7,19 +7,17 @@ def get_days_from_today(date):  # Ensure the input is a string in format "%Y-%m-
     try:
         # Converting from string to datetime object
         parsed_date = datetime.strptime(date, "%Y-%m-%d").date()
+        current_date = datetime.today().date()
+        dates_difference = current_date.toordinal() - parsed_date.toordinal()
+        print(dates_difference)
+        return dates_difference  # Returns the quantity of days, class 'int'
     except ValueError:
-        raise ValueError("Invalid date format. Please provide the date in 'YYYY-MM-DD' format.")
-
-    current_date = datetime.today().date()
-    dates_difference = (current_date - parsed_date).days
-    print(dates_difference)
-    return dates_difference  # Returns the quantity of days, class 'int'
+        print("Invalid date format. Please provide the date in 'YYYY-MM-DD' format.")
 
 
 # Test the function
-try:
-    get_days_from_today("2025-05-25")  # Valid input
-    get_days_from_today("2024-05-25")  # Valid input
-    get_days_from_today("2024.05.25")  # Invalid input
-except ValueError as mistake:
-    print(mistake)
+
+get_days_from_today("2025.05.25")  # Invalid input
+get_days_from_today("2025-05-25")  # Valid input
+get_days_from_today("2025/05/25")  # Invalid input
+get_days_from_today("2024-05-25")  # Valid input
--- hw_03_part_2.py
+++ hw_03_part_2.py
@@ -1,15 +1,23 @@
-import random
 from random import randint
 
-#Description: function is to generate a set of unique random numbers in the specified range
+
+# Description: function is to generate a set of unique random numbers in the specified range
 
 def get_numbers_ticket(min, max, quantity):
     result_list = set()
-    if min >= 1 and max <= 1000:
+    if min >= 1 and (max - min +1) >= quantity and max <= 1000:
         while len(result_list) != quantity:
             result_list.add(randint(min, max))
     print(sorted(list(result_list)))
-    return sorted(list(result_list)) #if success returns list of sorted unique numbers, otherwise - empty list
-    
-#test the function:
-get_numbers_ticket(10, 20, 5)
+    return sorted(list(result_list))  # if success returns list of sorted unique numbers, otherwise - empty list
+
+
+# test the function:
+
+get_numbers_ticket(10, 10, 1)
+get_numbers_ticket(1, 1000, 10)
+
+# incorrect variants:
+get_numbers_ticket(10, 4, 2) # (max_value is less than min_value)
+get_numbers_ticket(10, 15, 9) # (quantity is higher than range)
+get_numbers_ticket(10, 1001, 10) # (one of parameter is out of range)
